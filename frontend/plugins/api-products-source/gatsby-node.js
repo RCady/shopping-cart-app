edcdd4444960380f5f93f9d63c92815ad9d11353
RCady <rcady94@gmail.com>
2020-03-20 00:10:47 -0500

    Add source plugin for products api

---
+++ frontend/plugins/api-products-source/gatsby-node.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch')
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest }
+) => {
+  const { createNode } = actions
+
+  const url = 'http://127.0.0.1:8000/api/products'
+  const createProductNodeData = product => {
+    const nodeId = createNodeId(`product-${product.id}`)
+    const nodeContent = JSON.stringify(product)
+    const nodeData = Object.assign({}, product, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `Product`,
+        content: nodeContent,
+        contentDigest: createContentDigest(product),
+      },
+    })
+    return nodeData
+  }
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      data.data.forEach(product => {
+        const nodeData = createProductNodeData(product)
+        createNode(nodeData)
+      })
+    })
+}
