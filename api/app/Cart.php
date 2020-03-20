037d52273de6ff612eb039220f260357d9a6f63e
RCady <rcady94@gmail.com>
2020-03-19 22:52:40 -0500

    Add cart and cart item relationships

--- api/app/Cart.php
+++ api/app/Cart.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Cart\CartItem;
 
 class Cart extends Model
 {
-    //
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
--- api/app/Cart/CartItem.php
+++ api/app/Cart/CartItem.php
@@ -3,8 +3,12 @@
 namespace App\Cart;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Cart;
 
 class CartItem extends Model
 {
-    //
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
 }
