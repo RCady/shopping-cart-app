f641d269fb6e4e13a649367901962653d14060f9
RCady <rcady94@gmail.com>
2020-03-19 22:52:58 -0500

    Cleanup controllers

--- api/app/Http/Controllers/ProductController.php
+++ api/app/Http/Controllers/ProductController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $products = Product::all();
+        return response()->json([
+            'data' => $products
+        ]);
+    }
+
+    public function show(Request $request, Product $product)
+    {
+        return response()->json([
+            'data' => $product
+        ]);
+    }
 }
