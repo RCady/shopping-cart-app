ca3076bf19a1436a4a6bf5b23cbbc65046345173
RCady <rcady94@gmail.com>
2020-03-20 00:11:26 -0500

    Add products to index page for testing

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -14,8 +14,31 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <ul>
+    {
+      data.allProduct.edges.map(edge => <li>{ edge.node.name }</li> )
+    }
+    </ul>
+    { data.allProduct.edges[0].node.name }
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
+
+export const query = graphql`
+  query ProductsQuery {
+    allProduct {
+      edges {
+        node {
+          id
+          name
+          short_description
+          description
+          price
+          created_at
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
