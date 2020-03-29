91ba339c6b7381ef461b92422a776bd5c4c80820
RCady <rcady94@gmail.com>
2020-03-29 16:15:40 -0500

    Remove default content from index

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,26 +1,23 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import ProductCard from "../components/ProductCard"
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <ul>
-    {
-      data.allProduct.edges.map(edge => <li>{ edge.node.name }</li> )
-    }
-    </ul>
-    { data.allProduct.edges[0].node.name }
-    <Link to="/page-2/">Go to page 2</Link>
+    <Row className="row-eq-height mt-4">
+      {
+        data.allProduct.edges.map(edge => (
+            <Col xs="12" md="6" lg="4" style={{ marginBottom: 15 }} key={edge.node.id} >
+              <ProductCard product={edge.node}/>
+            </Col>
+          )
+        )
+      }
+    </Row>
   </Layout>
 )
 
