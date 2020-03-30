c8bfe1552cb38b97dd99f317541d47e945fc17b8
RCady <rcady94@gmail.com>
2020-03-30 12:57:00 -0500

    Ensure the application works both in the browser and when being built with SSR

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,1 +1,8 @@
-export { default as wrapRootElement } from "./src/state/ReduxWrapper"
+import React from "react"
+import ReduxWrapper from "./src/state/ReduxWrapper"
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ReduxWrapper element={ element } requestCart={ true }>{ element }</ReduxWrapper>
+  )
+}
--- frontend/src/state/ReduxWrapper.js
+++ frontend/src/state/ReduxWrapper.js
@@ -1,35 +1,40 @@
 import React from "react"
 import { Provider } from "react-redux"
 import thunkMiddleware from "redux-thunk"
-import { createStore, applyMiddleware, compose } from "redux"
+import { createStore, applyMiddleware } from "redux"
 import { fetchCart } from "./actions"
 import rootReducer from "./reducers"
 
-const initialState = {
-  cart: {
-    id: "",
-    items: [],
-    total_items: 0,
-    total_price: 0,
-    created_at: null,
-    updated_at: null,
-    is_loading: true,
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const ReduxWrapper = ({ children, requestCart }) => {
+  const initialState = {
+    cart: {
+      id: "",
+      items: [],
+      total_items: 0,
+      total_price: 0,
+      created_at: null,
+      updated_at: null,
+      is_loading: true,
+    }
   }
+
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(
+      thunkMiddleware
+    )
+  )
+
+  if (requestCart) {
+    store.dispatch(fetchCart(initialState.cart.id))
+  }
+
+  return (
+    <Provider store={ store }>{ children }</Provider>
+  )
 }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(
-    thunkMiddleware
-  ))
-)
-
-store.dispatch(fetchCart(initialState.cart.id))
-
-export default ({ element }) => (
-  <Provider
-    store={store}>{ element }</Provider>
-)
+export default ReduxWrapper
