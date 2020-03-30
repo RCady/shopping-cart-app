47727e091ae3910fc448db647b72fa1e56700a49
RCady <rcady94@gmail.com>
2020-03-30 10:49:22 -0500

    Update actions to now work with the API

--- frontend/src/state/actions.js
+++ frontend/src/state/actions.js
@@ -1,39 +1,59 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: `http://127.0.0.1:8000/api/`
+  baseURL: `http://127.0.0.1:8000/api/`,
+  headers: { "Accept": "application/json" }
 })
+
+export const handleCartResponse = (dispatch, response) => {
+  dispatch(setCart(response.data.data))
+  dispatch(setCartIsLoaded())
+}
 
 export const fetchCart = () => {
   return (dispatch) => {
-    dispatch(requestCart())
+    dispatch(setCartIsLoading())
 
-    return api.get(`cart`)
-      .then((response) => response.data)
-      .then((data) => dispatch(setCart(data.data)))
+    return api.get(`cart/c00b36af-b750-4f76-bc24-03fabd9f5f00`)
+      .then((response) => handleCartResponse(dispatch, response))
   }
 }
 
-export const requestCart = () => ({
-  type: "REQUEST_CART"
+export const removeItemFromCart = (cartItem) => {
+  return (dispatch) => {
+    return api.delete(`cart/c00b36af-b750-4f76-bc24-03fabd9f5f00/item/${cartItem.id}`)
+      .then((response) => handleCartResponse(dispatch, response))
+  }
+}
+
+export const addItemToCart = (product, qty) => {
+  return (dispatch) => {
+    return api.post(`cart/c00b36af-b750-4f76-bc24-03fabd9f5f00/item`, {
+      product_id: product.product_id,
+      qty,
+    })
+      .then((response) => handleCartResponse(dispatch, response))
+  }
+}
+
+export const updateCartItem = (cartItem, qty) => {
+  return (dispatch) => {
+    return api.put(`cart/c00b36af-b750-4f76-bc24-03fabd9f5f00/item/${cartItem.id}`, {
+      qty,
+    })
+      .then((response) => handleCartResponse(dispatch, response))
+  }
+}
+
+export const setCartIsLoading = () => ({
+  type: "CART_IS_LOADING"
+})
+
+export const setCartIsLoaded = () => ({
+  type: "CART_IS_LOADED"
 })
 
 export const setCart = (data) => ({
   type: "SET_CART",
   cart: data,
-})
-
-export const addItemToCart = (product, qty) => ({
-  type: "ADD_ITEM",
-  product,
-  qty,
-})
-
-export const removeItemFromCart = (index) => ({
-  type: "REMOVE_ITEM",
-  index,
-})
-
-export const updateItemQty = (cartItem) => ({
-  type: "UPDATE_ITEM_QTY",
 })
